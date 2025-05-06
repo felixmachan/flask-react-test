3ad0818bcdfac81b204a43789c2e356aa56e049e
felixmachan <126292501+felixmachan@users.noreply.github.com>
2025-05-06 22:59:09 +0200

    implement wavy hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import "../Hero.css"
-
+import "../Hero.css";
 
 function Hero(props) {
   const {
@@ -10,43 +9,46 @@ function Hero(props) {
     showButton = false,
     buttonText = "Időpontot foglalok",
     buttonAction = () => {},
-    href // <-- ÚJ: href támogatás
+    href
   } = props;
 
   return (
-    <div className="px-4 py-5 mb-3 text-center container-fluid hero w-100">
-      <div className="d-block mx-auto mb-4 mt-3" style={{ fontSize: "72px"}}>
-        {icon}
-      </div>
-      <h1 className="display-5 fw-bold text-body-emphasis">{title}</h1>
-      <div className="col-lg-6 mx-auto">
-        <p className="lead mb-5 mt-4">
-          {body}
-        </p>
+    <div className="hero-wrapper">
+      <div className="px-4 py-5 mb-0 text-center container-fluid hero w-100">
+        <div className="d-block mx-auto mb-4 mt-3" style={{ fontSize: "72px" }}>
+          {icon}
+        </div>
+        <h1 className="display-5 fw-bold text-body-emphasis">{title}</h1>
+        <div className="col-lg-6 mx-auto">
+          <p className="lead mb-5 mt-4">{body}</p>
 
-        {showButton && (
-          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            {href ? (
-              <a
-                href={href}
-                className="btn btn-primary btn-lg px-4 gap-3 blue-bg"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {buttonText}
-              </a>
-            ) : (
-              <button
-                type="button"
-                className="btn btn-primary btn-lg px-4 gap-3 blue-bg button"
-                onClick={buttonAction}
-              >
-                {buttonText}
-              </button>
-            )}
-          </div>
-        )}
+          {showButton && (
+            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+              {href ? (
+                <a
+                  href={href}
+                  className="btn btn-primary btn-lg px-4 gap-3 blue-bg"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {buttonText}
+                </a>
+              ) : (
+                <button
+                  type="button"
+                  className="btn btn-primary btn-lg px-4 gap-3 blue-bg button"
+                  onClick={buttonAction}
+                >
+                  {buttonText}
+                </button>
+              )}
+            </div>
+          )}
+        </div>
       </div>
+
+      {/* Hullámos alja (SVG) */}
+      <img src='../../public/stacked-waves-haikei.svg'/>
     </div>
   );
 }
